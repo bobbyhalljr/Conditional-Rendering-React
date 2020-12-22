392889c17e96ddbe73b57521852345dc37a88608
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-12-22 13:37:18 -0500

    adding-state

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,26 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import React from 'react'
 
 export default function Home() {
+  // Local State (true or false)
+  const [darkMode, setDarkMode] = React.useState(false)
+
+  // setting up vars
+  const dark = '#333'
+  const light = '#f1f1f1'
+
+  // styles
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Conditional Rendering in React</title>
+        <title>Conditional Rendering React</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        
+        {darkMode === true ? <div>Dark</div> : <div>Light</div>}
       </main>
     </div>
   )
